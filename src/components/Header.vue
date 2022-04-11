c2a069a0c90522ba8bdbe2791e0f68387be3e324
Denis24NN <newappianway87@gmail.com>
2022-04-11 13:15:26 +0300

    Округление до сотых сделано

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <h3>Товаров в корзине на: {{ cartPrice }} {{ currency }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    cart: Array,
+    required: true,
+    default: {},
+
+    currency: String,
+  },
+  data() {
+    return {
+      cartPrice: 0,
+    };
+  },
+  watch: {
+    cart(cart) {
+      let val = 0;
+      cart.forEach((item) => {
+        val += item.price * item.amount;
+      });
+      this.cartPrice = Math.round(val * 100) / 100;
+    },
+  },
+}
+</script>
+
+<style scoped>
+  .header {
+    padding: 10px;
+    background: #e5e5e5;
+  }
+</style>
+
