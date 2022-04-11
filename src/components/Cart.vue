749c8beadc96354251da80031f1bf742dda4be7e
Denis24NN <newappianway87@gmail.com>
2022-04-11 13:18:18 +0300

    Корзину сделал вторым элементом

---
+++ src/components/Cart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="cart-list">
+    <div v-for="item in spisok_dlya_rendera">
+    <span> {{ item.title }} - </span>
+    <span>   {{ item.price }} * </span>
+    <span>   {{ item.amount }} = </span>
+    <span>   {{ Math.round(item.amount * item.price * 100) / 100  }} {{ currency }} </span>
+     
+    
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cart: Array,
+    currency: String,
+  },
+  computed: {
+      spisok_dlya_rendera() {
+          return this.cart.reverse();
+      },
+  },
+};
+</script>
+
+<style>
+
+</style>
